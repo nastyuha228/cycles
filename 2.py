87a2c8117b429ff285b9be813cff548b808fe10b
nastyuha228 <143863640+nastyuha228@users.noreply.github.com>
2023-09-30 11:00:24 +0700

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,5 @@
+A = int(input())
+B = int(input())
+for num in range(A, B + 1):
+    c = num ** 3
+    print(c)
---
+++ 4.py
@@ -0,0 +1,5 @@
+n = int(iput())
+f = 1
+for i in range(1, n + 1):
+    f *= i
+print(f)
